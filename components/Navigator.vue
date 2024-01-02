7fa52b3894128f5a75d324185d5bded0f066ff08
Chi Nguyen <c.nguyen.prog@gmail.com>
2024-01-03 00:06:28 +0100

    feat: Add album title & description

--- components/Navigator.vue
+++ components/Navigator.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="">
-    <ChevronLeftIcon class="w-6 h-6" />
-  </div>
+  <NuxtLink :to="to">
+    <ChevronLeftIcon class="h-8 w-8 rounded-full p-1 hover:bg-primary/10" />
+  </NuxtLink>
 </template>
 <script setup lang="ts">
-import { ChevronLeftIcon } from '@heroicons/vue/24/outline'
-const route = useRoute()
-const path = computed(() => route.path.slice(1).split('/'))
+import { ChevronLeftIcon } from "@heroicons/vue/24/outline"
+
+defineProps<{ to: string }>()
 </script>
--- pages/album/[id].vue
+++ pages/album/[id].vue
@@ -1,23 +1,35 @@
 <script setup lang="ts">
 const route = useRoute()
-const albums = computed(() => useAlbumsStore().albums)
-const photos = computed(() => albums.value?.find((album) => album.id === route.params.id)?.photos)
+const albumStore = useAlbumsStore()
+const currentAlbum = computed(() => albumStore.albums.find((album) => album?.id === route?.params?.id))
+const photos = computed(() => currentAlbum.value?.photos)
 
 const photoStore = usePhotoStore()
 const active = useState()
 </script>
 
 <template>
-  <div class="container">
-    <figure v-for="(photo, index) of photos" :key="photo.id">
-      <NuxtLink :to="`/photo/${photo.id}`">
-        <Photo
-          :url="photo?.url"
-          @click="photoStore.photos = photos"
-          @click.native="active = photo.id"
-          :class="{ transition: index === 0, active: active === photo?.id }" />
-      </NuxtLink>
-    </figure>
+  <div class="flex flex-col gap-4">
+    <section class="flex flex-col gap-4">
+      <div class="flex flex-row items-center gap-2">
+        <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-100">{{ currentAlbum?.title }}</h1>
+      </div>
+
+      <p class="text-gray-500 dark:text-gray-400">
+        {{ currentAlbum?.description }}
+      </p>
+    </section>
+    <div class="container">
+      <figure v-for="(photo, index) of photos" :key="photo.id">
+        <NuxtLink :to="`/photo/${photo.id}`">
+          <Photo
+            :url="photo?.url"
+            @click="photoStore.photos = photos"
+            @click.native="active = photo.id"
+            :class="{ transition: index === 0, active: active === photo?.id }" />
+        </NuxtLink>
+      </figure>
+    </div>
   </div>
 </template>
 <style scoped>
